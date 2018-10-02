a12623b95c4c6b6819d258da2abe567318395afe
HCarlos <r0@tecnointel.mx>
2018-10-02 14:39:26 -0500

    PS - Aw1 | Alfa

--- resources/views/shared/user/__user_becas_edit.blade.php
+++ resources/views/shared/user/__user_becas_edit.blade.php
@@ -1,28 +1,28 @@
 <div class="form-group has-error row mb-3">
     <label for = "beca_sep" class="col-md-3 col-form-label">Beca SEP</label>
     <div class="col-md-9">
-        <input type="text" name="beca_sep" id="beca_sep" value="{{ old('beca_sep',$items->user_becas->beca_sep) }}" class="form-control" />
+        <input type="text" name="beca_sep" id="beca_sep" value="{{ old('beca_sep',$items->user_becas->beca_sep) }}" pattern="^\d+(\.\d{1,2})?$" class="form-control" />
         <span class="has-error has-beca_sep">
             <strong class="text-danger"></strong>
         </span>
     </div>
     <label for = "beca_arji" class="col-md-3 col-form-label">Beca Arjí</label>
     <div class="col-md-9">
-        <input type="text" name="beca_arji" id="beca_arji" value="{{ old('beca_arji',$items->user_becas->beca_arji) }}" class="form-control" />
+        <input type="text" name="beca_arji" id="beca_arji" value="{{ old('beca_arji',$items->user_becas->beca_arji) }}" pattern="^\d+(\.\d{1,2})?$" class="form-control" />
         <span class="has-error has-beca_arji">
             <strong class="text-danger"></strong>
         </span>
     </div>
     <label for = "beca_spf" class="col-md-3 col-form-label">Beca SPF</label>
     <div class="col-md-9">
-        <input type="text" name="beca_spf" id="beca_spf" value="{{ old('beca_spf',$items->user_becas->beca_spf) }}" class="form-control" />
+        <input type="text" name="beca_spf" id="beca_spf" value="{{ old('beca_spf',$items->user_becas->beca_spf) }}" pattern="^\d+(\.\d{1,2})?$" class="form-control" />
         <span class="has-error has-beca_spf">
             <strong class="text-danger"></strong>
         </span>
     </div>
     <label for = "beca_bach" class="col-md-3 col-form-label">Beca Bach.</label>
     <div class="col-md-9">
-        <input type="text" name="beca_bach" id="beca_bach" value="{{ old('beca_bach',$items->user_becas->beca_bach) }}" class="form-control" />
+        <input type="text" name="beca_bach" id="beca_bach" value="{{ old('beca_bach',$items->user_becas->beca_bach) }}" pattern="^\d+(\.\d{1,2})?$" class="form-control" />
         <span class="has-error has-beca_bach">
             <strong class="text-danger"></strong>
         </span>
